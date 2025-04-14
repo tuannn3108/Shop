4af602173e69963a4c2b633609ee532ea734a12e
tuannn3108 <anhtuan3182002222@gmail.com>
2025-04-14 15:05:01 +0700

    init

--- BE/config/config.js
+++ BE/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  SECRET_KEY: "NNPTUD_S6",
+  SECRET_KEY: "",
   EXP_JWT: "1d",
   Host: "smtp.gmail.com",
   Port: 587,
-  Username: "songhuy3042002@gmail.com",
+  Username: "anhtuan3182002222@gmail.com",
   Password: "xlbw sbdd mccr pnmm",
 };
